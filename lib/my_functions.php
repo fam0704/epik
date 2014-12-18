02dd61bc8c8b8c0bc68dd46c59185e261c7c16f6
Fredy Arevalo <fam0704@gmail.com>
2014-12-18 12:02:15 -0500

    Siigo
    Theme 2015 Siigo

---
+++ lib/my_functions.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * My Custom Functions
+*/
+
+//* Add support for custom header
+
+add_theme_support( 'custom-header', array(
+
+	'width'           => 184,
+
+	'height'          => 91,
+
+	'header-selector' => '.site-title a',
+
+	'header-text'     => false,
+
+) );
+
+
+
+//*
+
+
+//* Customize footer the credits
+
+add_filter( 'genesis_footer_creds_text', 'sp_footer_creds_text' );
+
+function sp_footer_creds_text() {
+
+	echo '<div class="creds"><p>';
+
+	echo 'Copyright &copy; ';
+
+	echo date('Y');
+
+	echo ' &middot; <a href="http://http://siigo.net/software/" title="Siigo.com">Siigo.com</a> &middot; Todos los derechos reservados ';
+
+	echo '</p></div>';
+
+}
---
+++ page_full.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file adds the Landing template to the Epik Pro Theme.
+ *
+ * @author StudioPress
+ * @package Epik
+ * @subpackage Customizations
+ */
+
+/*
+Template Name: Full
+*/
+
+//* Run the Genesis loop
+genesis();
